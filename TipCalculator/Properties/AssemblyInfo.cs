4028d8863ed00f9171707905d2c44d6fc999dcd2
O'Dale Cotterell <ocotterell@gmail.com>
2017-08-23 00:37:30 -0400

    title update

--- TipCalculator/Properties/AssemblyInfo.cs
+++ TipCalculator/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TipCalculator")]
+[assembly: AssemblyTitle("Tip Calculator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TipCalculator")]
+[assembly: AssemblyProduct("Tip Calculator")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
